74ea0739535051d3d4a1d1b11fdb4bc3bd65b7b6
smedziko <smedziko@htl-steyr.ac.at>
2022-03-03 14:14:53 +0100

    IllegalDateException-Klasse

--- src/htbla/steyr/IllegalDateException.java
+++ src/htbla/steyr/IllegalDateException.java
@@ -1,4 +1,22 @@
 package htbla.steyr;
-
+/*----------------------------------------------------------------------
+ *            Hoehere Technische Bundeslehranstalt STEYR
+ *---------------------------------------------------------------------*/
+/**
+ * Kurzbeschreibung
+ *
+ * @author  : Semir Medzikovic
+ *            Schueler, 3AHITN
+ * @date    : 3.März 2022
+ * File:   IllegalDateException.java
+ * @detalis: IllegalDateException-Klasse
+ */
 public class IllegalDateException {
+    protected int type;
+    public int DAY_ILLEGAL;
+    public int MON_ILLEGAL;
+
+    public IllegalDateException(int i){
+
+    }
 }
