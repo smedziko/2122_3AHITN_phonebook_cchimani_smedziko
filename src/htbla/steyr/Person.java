0b2463239d4b3c5474d1cc1a60a16bf6607a5b1c
smedziko <smedziko@htl-steyr.ac.at>
2022-03-03 14:23:44 +0100

    Person.java

--- src/htbla/steyr/Person.java
+++ src/htbla/steyr/Person.java
@@ -1,5 +1,16 @@
 package htbla.steyr;
-
+/*----------------------------------------------------------------------
+ *            Hoehere Technische Bundeslehranstalt STEYR
+ *---------------------------------------------------------------------*/
+/**
+ * Kurzbeschreibung
+ *
+ * @author  : Semir Medzikovic
+ *            Schueler, 3AHITN
+ * @date    : 3.März 2022
+ * File:   Person.java
+ * @detalis: Person-Klasse
+ */
 public class Person {
     protected String name;
     protected String givenname;
