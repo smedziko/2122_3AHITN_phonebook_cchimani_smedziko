197b031ac2b3c27f69960189e49c16973d06e175
smedziko <smedziko@htl-steyr.ac.at>
2022-03-03 14:23:20 +0100

    Person.java

--- src/htbla/steyr/Person.java
+++ src/htbla/steyr/Person.java
@@ -1,4 +1,11 @@
 package htbla.steyr;
 
 public class Person {
+    protected String name;
+    protected String givenname;
+    protected String nickname;
+    protected Date birthday;
+    protected PhoneNumber phone;
+
+    public Person(String n, String gn, String bd, String p)
 }
