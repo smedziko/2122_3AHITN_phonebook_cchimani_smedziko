743fac92fee9bc712702d7be6ed30082bc0ac0ef
smedziko <smedziko@htl-steyr.ac.at>
2022-03-04 08:46:50 +0100

    don't know why

--- src/htbla/steyr/PhoneNumber.java
+++ src/htbla/steyr/PhoneNumber.java
@@ -11,19 +11,32 @@ package htbla.steyr;
  * File:   PhoneNumber.java
  * @detalis: PhoneNumber-Klasse
  */
+
+/**
+ * PhoneNumber - Klasse, welche das Land, das Gebiet und die Nummer beinhaltet <br>
+ */
 public class PhoneNumber {
     protected int country;
     protected int area;
     protected int number;
 
+    /**
+     * Konstruktor für country, area und number
+     */
     PhoneNumber(int c, int a, int n){
 
     }
+
+    /**
+     * Konstruktor für number
+     */
     PhoneNumber(String number){
 
     }
 
-    @Override
+    /**
+     * wandelt alles in einen String um
+     */
     public String toString() {
         return "PhoneNumber{" +
                 "country=" + country +
@@ -31,20 +44,28 @@ public class PhoneNumber {
                 ", number=" + number +
                 '}';
     }
-
-
+    /**
+     * Getter für Number
+     */
     public int getNumber(){
         return 0;
     }
-
-
+    /**
+     * Getter für CountryCode
+     */
     public int getCountryCode() {
         return country;
     }
-
+    /**
+     * Getter für AreaCode
+     */
     public int getAreaCode() {
         return area;
     }
+
+    /**
+     * prüft, ob PhoneNumber gültig ist
+     */
     public boolean isValid(PhoneNumber d){
         return true;
     }
