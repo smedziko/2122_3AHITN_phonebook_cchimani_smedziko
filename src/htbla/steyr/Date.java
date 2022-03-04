14185f52ec0a71f539d2a8048131493dc191f1c7
smedziko <smedziko@htl-steyr.ac.at>
2022-03-04 08:37:20 +0100

    don't know why

--- src/htbla/steyr/Date.java
+++ src/htbla/steyr/Date.java
@@ -12,30 +12,49 @@ package htbla.steyr;
  * @detalis: Date-Klasse
  */
 
+/**
+ * Date - Klasse, welche Tag, Monat und Jahr definiert <br>
+ */
 public class Date {
     protected int day;
     protected int mon;
     protected int year;
 
+    /**
+     * isLeapYear: prüft, ob es ein Schaltjahr ist <br>
+     */
     public boolean isLeapYear(){
         return false;
     }
+
+    /**
+     * isValid: prüft, ob das Datum gültig ist <br>
+     */
     public boolean isValid(Date d){
         return false;
     }
-
+    /**
+     * public Date: Konstruktor für Tag, Monat, Jahr <br>
+     */
     public Date (int m, int d, int y){
 
     }
 
+    /**
+     * Getter für den Tag
+     */
     public int getDay() {
         return day;
     }
-
+    /**
+     * Getter für den Monat
+     */
     public int getMon() {
         return mon;
     }
-
+    /**
+     * Getter für das Jahr
+     */
     public int getYear() {
         return year;
     }
